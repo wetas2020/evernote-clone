714a62e07c0fd6102dc056a4429063e15d1bd312
Saad El Gabouri <elgabouri.saad@gmail.com>
2022-08-05 12:45:21 +0100

    add some functions to sidebaritem

--- src/sidebarItem/sidebarItem.js
+++ src/sidebarItem/sidebarItem.js
@@ -7,13 +7,41 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import { removeHTMLTags } from '../helpers';
 
 class SidebarItemComponent extends React.Component {
-    constructor() {
-        super();
+    render() {
+        const { classes, note, index, selectedNoteIndex } = this.props;
+        return (
+            <div key={index}>
+                <ListItem
+                    className={classes.ListItem}
+                    selected={selectedNoteIndex === index}
+                    alignItems="flex-start"
+                >
+                    <div
+                        className={classes.textSection}
+                        onClick={() => this.selectNote(note, index)}
+                    >
+                        <ListItemText
+                            primary={note.title}
+                            secondary={removeHTMLTags(note.body.substring(0, 30)) + '...'}
+                        ></ListItemText>
+                    </div>
+                    <DeleteIcon
+                        onClick={() => this.deleteNote(note)}
+                        className={classes.deleteIcon}
+                    ></DeleteIcon>
+                </ListItem>
+            </div>
+        );
     }
 
-    render() {
-        return <div>Hello from the SidebarItemComponent</div>;
-    }
+    selectNote = (note, index) => {
+        this.props.selectNote(note, index);
+    };
+    deleteNote = (note) => {
+        if (window.confirm(`Are You Sure You Want To Delete: ${note.title}`)) {
+            this.props.deleteNote(note);
+        }
+    };
 }
 
 export default withStyles(styles)(SidebarItemComponent);
